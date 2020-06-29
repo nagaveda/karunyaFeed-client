0f4e86daf907e8e35d864fd177c623bad4e7ea6b
nagaveda <gajjalanagaveda@karunya.edu.in>
2020-06-29 10:31:21 +0530

    Adding context api

--- src/components/screens/Signin.js
+++ src/components/screens/Signin.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {Link, useHistory} from 'react-router-dom'; 
 import M from 'materialize-css';
+import {UserContext} from '../../App';
 const Signin = () => {
-    const history = useHistory();
+    const {state, dispatch} = useContext(UserContext);    const history = useHistory();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const postData = () => {
@@ -26,9 +27,9 @@ const Signin = () => {
                 M.toast({html: data.error, classes:"#c62828 red darken-3"})
             }
             else{
-                console.log("test: ", data.token);
                 localStorage.setItem("jwt", data.token);
-                localStorage.setItem("user", data.user);
+                localStorage.setItem("user", JSON.stringify(data.user));
+                dispatch({type: "USER", payload: data.user});
                 M.toast({html:"Signed in successfully!", classes:"#1de9b6 teal accent-3"})
                 history.push('/');
             }
